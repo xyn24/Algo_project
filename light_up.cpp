75f728f87a04cd1b20ed10a8362e4083f7b5b4e3
Xu YiNuo <xyn24@mails.tsinghua.edu.cn>
2024-10-18 16:05:59 +0800

    Add files via upload

--- light_up.cpp
+++ light_up.cpp
@@ -7,6 +7,11 @@
 #include <sstream>
 #include <string>
 #include <regex>
+#include <utility>
+
+#define MAXN 1024
+#define MAXEDGES 10000
+#define MAXNB 100
 
 using namespace std;
 
@@ -14,69 +19,64 @@ int len = 1000, init_len = 1000;
 const int repeat_times_bound = 1000;
 int repeat_times = 0;
 double sum = 0, sum2 = 0;
-const double T_0 = 100, cooling_rate = 1 - 1e-4, T_min = 0.01; 
+const double T_0 = 100, cooling_rate = 1 - 1e-4, T_min = 0.01;
 double loss = 100000, loss_num = 100000;
 int num = 0, sum_num = 0;
-const double p_max = 6, p_min = 6, p_step = 1;
+const double p_max = 6, p_min = 0, p_step = 1;
 double p = 0, temp_p = 0, init_p = 0;
-bool is_init = false;  //change this to initialize the p
+bool is_init = false; // change this to initialize the p
 bool is_stable = false;
 
 struct vertex
 {
     int state;
-    vector<int> neighbor;
-    vertex(): state(rand()%2), neighbor(vector<int>()) {}
-};
-
-struct edge
-{
-    bool is_exist = false;
-    int value = false;
+    int neighbor[MAXNB];
+    int size;
+    vertex() : state(rand() % 2), size(0) {}
 };
 
 struct graph
 {
-    vector<vertex> vertices;
-    vector<pair<int, int>> edges;
-    vector<vector<edge>> edge_matrix;
+    vertex vertices[MAXN];
+    int edges[MAXEDGES][2];
+    int vertices_size, edge_size;
+    bool edge_matrix[MAXN][MAXN];
     double loss_1, loss_21, loss_31;
 
     void add_edge(int u, int v)
     {
-        vertices[u].neighbor.push_back(v);
-        vertices[v].neighbor.push_back(u);
-        edges.push_back(make_pair(u, v));
-        edge_matrix[u][v].is_exist = true;
-        edge_matrix[v][u].is_exist = true;
+        vertices[u].neighbor[vertices[u].size++] = v;
+        vertices[v].neighbor[vertices[v].size++] = u;
+        edges[edge_size][0] = u;
+        edges[edge_size][1] = v;
+        edge_size++;
+        edge_matrix[u][v] = true;
+        edge_matrix[v][u] = true;
     }
 
-    graph(int n): vertices(vector<vertex>(n))
+    graph(int n) : vertices_size(n), edge_size(0), loss_1(0), loss_21(0), loss_31(0)
     {
-        edge_matrix = vector<vector<edge>>(n, vector<edge>(n));
+        fill(vertices, vertices + n, vertex());
+        for (int i = 0; i < n; ++i)
+            fill(edge_matrix[i], edge_matrix[i] + n, false);
         for (int i = 0; i < n; ++i)
         {
             for (int j = i + 1; j < n; ++j)
             {
-                if (((double)rand() / RAND_MAX)<0.01)
+                if (((double)rand() / RAND_MAX) < 0.01)
                 {
                     add_edge(i, j);
                 }
             }
         }
-        loss_1 = 0;
-        loss_21 = 0;
-        loss_31 = 0;
-        for (vertex v: vertices)
+        for (int i = 0; i < vertices_size; ++i)
         {
-            loss_1 += v.state;
+            loss_1 += vertices[i].state;
         }
-        for (pair<int, int> e: edges)
+        for (int i = 0; i < edge_size; ++i)
         {
-            edge_matrix[e.first][e.second].value = vertices[e.first].state^vertices[e.second].state;
-            edge_matrix[e.second][e.first].value = vertices[e.first].state^vertices[e.second].state;
-            loss_21 += double(edge_matrix[e.first][e.second].value);
-            loss_31 += double(edge_matrix[e.first][e.second].value) * (1.0 / vertices[e.first].neighbor.size() + 1.0 / vertices[e.second].neighbor.size());
+            loss_21 += double(vertices[edges[i][0]].state ^ vertices[edges[i][1]].state);
+            loss_31 += double(vertices[edges[i][0]].state ^ vertices[edges[i][1]].state) * (1.0 / vertices[edges[i][0]].size + 1.0 / vertices[edges[i][1]].size);
         }
     }
 
@@ -84,36 +84,36 @@ struct graph
     {
         vertices[u].state = 1 - vertices[u].state;
         loss_1 += 2 * vertices[u].state - 1;
-        for (int v: vertices[u].neighbor)
+        for (int i = 0; i < vertices[u].size; ++i)
         {
+            int v = vertices[u].neighbor[i];
             vertices[v].state = 1 - vertices[v].state;
             loss_1 += 2 * vertices[v].state - 1;
         }
-        for (int v: vertices[u].neighbor)
-            for (int w: vertices[v].neighbor)
+        for (int i = 0; i < vertices[u].size; ++i)
+        {
+            int v = vertices[u].neighbor[i];
+            for (int j = 0; j < vertices[v].size; ++j)
             {
-                if (w != u)
+                int w = vertices[v].neighbor[j];
+                if (w != u && !edge_matrix[u][w])
                 {
-                    if (edge_matrix[v][w].value != (vertices[v].state^vertices[w].state))
-                    {
-                        edge_matrix[v][w].value = 1 - edge_matrix[v][w].value;
-                        edge_matrix[w][v].value = 1 - edge_matrix[w][v].value;
-                        loss_21 += 2 * (edge_matrix[v][w].value) - 1;
-                        loss_31 += (2 * (edge_matrix[v][w].value) - 1) * (1.0 / vertices[v].neighbor.size() + 1.0 / vertices[w].neighbor.size());
-                    }
+                    loss_21 += 2 * (vertices[v].state ^ vertices[w].state) - 1;
+                    loss_31 += (2 * (vertices[v].state ^ vertices[w].state) - 1) * (1.0 / vertices[v].size + 1.0 / vertices[w].size);
                 }
             }
+        }
     }
 
     void print()
     {
-        for (vertex v: vertices)
+        for (vertex v : vertices)
         {
             cout << v.state << " ";
         }
         cout << endl;
     }
-}g(0);
+} g(0);
 
 void gradient_descent()
 {
@@ -175,11 +175,11 @@ void main_algorithm()
         }
         if (T < 1 && T > 0.1)
         {
-            temp_p *= 1-2e-5;
+            temp_p *= 1 - 2e-5;
         }
         if (T < 0.1)
         {
-            temp_p *= 1-1e-4;
+            temp_p *= 1 - 1e-4;
         }
     }
     loss_num = g.loss_1;
@@ -200,7 +200,7 @@ void repeat_algorithm()
     {
         main_algorithm();
         if (repeat_times >= 10)
-            if (sum2 / (repeat_times*(repeat_times-1)) - ((sum*sum) / (repeat_times*repeat_times*(repeat_times-1))) < (sum / repeat_times * 0.005) * (sum / repeat_times * 0.005))
+            if (sum2 / (repeat_times * (repeat_times - 1)) - ((sum * sum) / (repeat_times * repeat_times * (repeat_times - 1))) < (sum / repeat_times * 0.002) * (sum / repeat_times * 0.002))
             {
                 is_stable = true;
                 break;
@@ -208,34 +208,43 @@ void repeat_algorithm()
     }
 }
 
-void extract_last_entry(int& len_value, double& p_value) {
+void extract_last_entry(int &len_value, double &p_value)
+{
     ifstream infile("light_output.txt");
-    if (!infile) {
+    if (!infile)
+    {
         cerr << "无法打开文件 light_output.txt" << endl;
         return;
     }
 
     string line;
     string last_line;
-    while (getline(infile, line)) {
-        if (!line.empty()) {
+    while (getline(infile, line))
+    {
+        if (!line.empty())
+        {
             last_line = line;
         }
     }
 
     infile.close();
 
-    if (last_line.empty()) {
+    if (last_line.empty())
+    {
         cerr << "文件为空或没有有效数据" << endl;
         return;
     }
 
     istringstream iss(last_line);
     string part;
-    while (getline(iss, part, '\t')) {
-        if (part.find("len:") != string::npos) {
+    while (getline(iss, part, '\t'))
+    {
+        if (part.find("len:") != string::npos)
+        {
             len_value = stod(part.substr(part.find(":") + 1));
-        } else if (part.find("p:") != string::npos) {
+        }
+        else if (part.find("p:") != string::npos)
+        {
             p_value = stod(part.substr(part.find(":") + 1));
         }
     }
@@ -248,20 +257,21 @@ int main()
     extract_last_entry(init_len, init_p);
 
     ofstream outfile("light_output.txt", std::ios::app);
-    if (!outfile) {
+    if (!outfile)
+    {
         cerr << "无法打开文件 light_output.txt" << endl;
         return 1;
     }
-    
-    for (len = 1000; len<=1000; len+=10)
+
+    for (len = 1000; len <= 1000; len += 10)
     {
-        for (p = p_min; p <= p_max+1e-6; p += p_step)
+        for (p = p_min; p <= p_max + 1e-6; p += p_step)
         {
             if (is_init)
             {
-                p = init_p+p_step;
+                p = init_p + p_step;
                 is_init = false;
-                if (p>p_max)
+                if (p > p_max)
                     break;
             }
             repeat_algorithm();
@@ -272,7 +282,7 @@ int main()
             }
             else
             {
-                outfile << "\tnot stable: " << (sqrt(sum2 / (repeat_times*(repeat_times-1)) - ((sum*sum) / (repeat_times*repeat_times*(repeat_times-1))))/(sum / repeat_times)) << endl;
+                outfile << "\tnot stable: " << (sqrt(sum2 / (repeat_times * (repeat_times - 1)) - ((sum * sum) / (repeat_times * repeat_times * (repeat_times - 1)))) / (sum / repeat_times)) << endl;
             }
         }
     }
